681b0a9add2221e402c929236c6032c545278edd
memory4963 <bolo4963@gmail.com>
2018-06-24 21:47:08 +0800

    init LSTM network, fix ReadData output

--- LSTM.py
+++ LSTM.py
@@ -2,16 +2,66 @@ import tensorflow as tf
 import numpy as np
 from tensorflow.contrib import rnn
 from tensorflow.examples.tutorials.mnist import input_data
+import ReadData
 
-# config = tf.ConfigProto()
-# config.gpu_options.allow_growth = True
-# sess = tf.Session(config=config)
-#
+config = tf.ConfigProto()
+config.gpu_options.allow_growth = True
+sess = tf.Session(config=config)
+
 # mnist = input_data.read_data_sets('MNIST_data', one_hot=True)
 # print(type(mnist.train.images))
-# s = "1.1 2.2 3.3 " \
-#     "4.4 5.5 6.6"
-# print(np.fromstring(s, dtype=np.float32, sep=" "))
-arr = np.array([[1, 2, 3], [4, 5, 6]])
-print(arr.tostring('C'))
-arr2 = np.fromstring(arr.tostring())
+
+skeleton, labels = ReadData.read_data("/home/luoao/openpose/dataset/simpleOutput")
+
+# 学习速率
+lr = 1e-3
+batch_size = tf.placeholder(tf.int32, [])
+# 每帧36个数
+input_size = 36
+# 每个batch60帧
+timestep_size = ReadData.batch_size
+# 每个隐藏层节点数
+hidden_size = 256
+# LSTM layer数
+layer_num = 2
+# 输出class数量
+class_num = 4
+
+x = tf.placeholder(tf.float32, [None, timestep_size, input_data])
+label = tf.placeholder(tf.int64, [None, class_num])
+keep_prob = tf.placeholder(tf.float32, [])
+
+lstm_cell = rnn.BasicLSTMCell(num_units=hidden_size, forget_bias=1.0, state_is_tuple=True)
+lstm_cell = rnn.DropoutWrapper(cell=lstm_cell, input_keep_prob=1.0, output_keep_prob=keep_prob)
+mlstm_cell = rnn.MultiRNNCell([lstm_cell] * layer_num, state_is_tuple=True)
+init_state = mlstm_cell.zero_state(batch_size, tf.float32)
+
+outputs, state = tf.nn.dynamic_rnn(mlstm_cell, inputs=x, initial_state=init_state, time_major=False)
+h_state = outputs[:, -1, :]
+
+# softmax
+weights = tf.Variable(tf.truncated_normal([hidden_size, class_num], stddev=0.1), dtype=tf.float32)
+bias = tf.Variable(tf.constant(0.1), dtype=tf.float32)
+y = tf.nn.softmax(tf.matmul(h_state, weights) + bias)
+
+cross_entropy = -tf.reduce_mean(label * tf.log(y))
+optimizer = tf.train.AdamOptimizer(lr).minimize(cross_entropy)
+
+correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(label, 1))
+accuracy = tf.reduce_mean(tf.cast(correct_prediction), "float")
+
+sess.run(tf.global_variables_initializer())
+for i in range(1000):
+    _batch_size = 32
+    train_batch = skeleton[i * 32:i * 32 + 32, :, :]
+    train_labels = labels[i * 32:i * 32 + 32]
+    if i + 1 % 10 == 0:
+        train_accuracy = sess.run(accuracy, feed_dict={
+            x: train_batch, label: train_labels,
+            keep_prob: 1.0, batch_size: _batch_size
+        })
+        print("train step %d, accuracy: %d" % i, train_accuracy)
+    sess.run(optimizer, feed_dict={
+        x: train_batch, label: train_labels,
+        keep_prob: 1.0, batch_size:skeleton.shape[0]
+    })
--- ReadData.py
+++ ReadData.py
@@ -15,19 +15,36 @@ def get_type(name: str):
 
 
 def read_data(path: str):
-    # label array: 1-dim array, every num reflects to 60 lines in action array.
+    # label array: 1-dim array, every num reflects to 60 lines in action array. [-1]
     label_temp = np.array([1])
-    # action array: 60 lines for each action data,
+    # action array: 60 lines for each action data. [-1, 60, 36]
     skeleton_temp = np.ones(shape=(1, 36), dtype=np.float32)
+
+    # 1 3 9 12 现共4类，之后增加
+    def a1():
+        return 0
+
+    def a3():
+        return 1
+
+    def a9():
+        return 2
+
+    def a12():
+        return 3
+
+    selector = {1: a1(), 3: a3(), 9: a9(), 12: a12()}
+
     for root, dirs, files in os.walk(path):
         for name in files:
             file_path = os.path.join(root, name)
             file_data = np.loadtxt(file_path, np.float32)
             for batch in range(batch_size, file_data.shape[0], batch_interval):
                 # label
-                label_temp = np.append(label_temp, get_type(name))
+                label_temp = np.append(label_temp, selector(get_type(name)))
                 # data
                 skeleton_temp = np.concatenate((skeleton_temp, file_data[batch - batch_size:batch, :]), axis=0)
     labels = label_temp[1:]
     skeleton = skeleton_temp[1:, :]
+    skeleton = skeleton.reshape([-1, 60, 36])
     return skeleton, labels
