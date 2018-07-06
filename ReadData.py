40da5357969e1a483ece7677c0ffe7650315b3d0
memory4963 <bolo4963@gmail.com>
2018-07-07 01:28:21 +0800

    change readData

--- ReadData.py
+++ ReadData.py
@@ -29,28 +29,31 @@ def read_data(path):
 
     # 1 3 9 12
     files = os.walk(path)
-    random.shuffle(files)
-    for root, dirs, files in files:
-        for index, name in enumerate(files):
-            file_path = os.path.join(root, name)
-            print("file count: " + str(index) + " / " + str(len(files)))
-            file_data = np.loadtxt(file_path, np.float32)
-            for batch in range(batch_size, file_data.shape[0], batch_interval):
-                # label
-                type_num = get_type(name)
-                if type_num == 1:
-                    label_temp = np.append(label_temp, 0)
-                elif type_num == 3:
-                    label_temp = np.append(label_temp, 1)
-                elif type_num == 9:
-                    label_temp = np.append(label_temp, 2)
-                elif type_num == 12:
-                    label_temp = np.append(label_temp, 3)
-                else:
-                    print("unexpected label type " + name)
-                    exit(1)
-                # data
-                skeleton_temp = np.concatenate((skeleton_temp, file_data[batch - batch_size:batch, :]), axis=0)
+    file_list = []
+    for root, dirs, file in files:
+        for name in file:
+            file_list.append([root, name])
+    random.shuffle(file_list)
+    for index, [root, name] in enumerate(file_list):
+        print("file count: " + str(index) + " / " + str(len(file_list)))
+        file_path = os.path.join(root, name)
+        file_data = np.loadtxt(file_path, np.float32)
+        for batch in range(batch_size, file_data.shape[0], batch_interval):
+            # label
+            type_num = get_type(name)
+            if type_num == 1:
+                label_temp = np.append(label_temp, 0)
+            elif type_num == 3:
+                label_temp = np.append(label_temp, 1)
+            elif type_num == 9:
+                label_temp = np.append(label_temp, 2)
+            elif type_num == 12:
+                label_temp = np.append(label_temp, 3)
+            else:
+                print("unexpected label type " + name)
+                exit(1)
+            # data
+            skeleton_temp = np.concatenate((skeleton_temp, file_data[batch - batch_size:batch, :]), axis=0)
     labels = label_temp[1:]
     skeleton = skeleton_temp[1:, :]
     skeleton = skeleton.reshape([-1, 60, 36])
