182413df38587ab4651107a95b8a7d9cfe0ed914
memory4963 <bolo4963@gmail.com>
2018-08-20 21:12:09 +0800

    change ts to saved_model

--- UsageTest.py
+++ UsageTest.py
@@ -1,19 +1,21 @@
 import tensorflow as tf
-import numpy as np
+# import numpy as np
 
 if __name__ == '__main__':
     with tf.Session as sess:
-        saver = tf.train.import_meta_graph('/home/luoao/openpose/models/model_999.ckpt.meta')
-        saver.restore(sess, tf.train.latest_checkpoint('/home/luoao/openpose/models/'))
+        saver = tf.train.import_meta_graph('D:\\ActionBasedFX\\origin_output\\model_950.ckpt.meta')
+        saver.restore(sess, tf.train.latest_checkpoint('D:\\ActionBasedFX\\origin_output\\'))
 
         graph = tf.get_default_graph()
-        x = graph.get_tensor_by_name('x:0')
-        label = graph.get_tensor_by_name('label:0')
+        x0 = graph.get_tensor_by_name('x0:0')
+        x1 = graph.get_tensor_by_name('x1:0')
+        x5 = graph.get_tensor_by_name('x5:0')
+        x10 = graph.get_tensor_by_name('x10:0')
         batch_size = graph.get_tensor_by_name('batch_size:0')
         keep_prob = graph.get_tensor_by_name('keep_prob:0')
         y = graph.get_tensor_by_name('y:0')
-        feed_dict = {x: np.arange(36*24).reshape(1, 24, 36),
-                     label: np.zeros(4),
-                     batch_size: 1,
-                     keep_prob: 1.0}
-        print(sess.run(y, feed_dict=feed_dict))
+
+        tf.saved_model.simple_save(sess, 'D:\\ActionBasedFX\\origin_output\\saved_model',
+                                   inputs={'x0': x0, 'x1': x1, 'x5': x5, 'x10': x10,
+                                           'batch_size': batch_size, 'keep_prob': keep_prob},
+                                   outputs={'y': y})
